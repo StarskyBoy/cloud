1b2a409f9059655d482037d290fee752f606319f
StarskyBoy <2254493426@qq.com>
2018-09-04 19:58:55 +0800

    ribbon 负载均衡

---
+++ cloud-register-consumer-ribbon/src/main/java/com/midou/cloud/register/ribbon/Controller/ConsumerController.java
@@ -0,0 +1,39 @@
+package com.midou.cloud.register.ribbon.Controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @author midou
+ * @description
+ * @date 2018/9/3 22:03
+ * @modified by
+ */
+@RestController
+public class ConsumerController {
+
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ConsumerController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+       ServiceInstance serviceInstance=this.loadBalancerClient.choose("cloud-register-user");
+         logger.info("/////////////////log-user-instance/////////////////////");
+        logger.info("{},{},{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance.getPort());
+    }
+}
