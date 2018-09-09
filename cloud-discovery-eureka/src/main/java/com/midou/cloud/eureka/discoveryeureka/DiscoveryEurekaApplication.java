9d6efda3339060909fad5d2b4964a562c6df3324
StarskyBoy <2254493426@qq.com>
2018-09-09 15:30:57 +0800

    开发工具使用IDEA

--- cloud-discovery-eureka/src/main/java/com/midou/cloud/eureka/discoveryeureka/DiscoveryEurekaApplication.java
+++ cloud-discovery-eureka/src/main/java/com/midou/cloud/eureka/discoveryeureka/DiscoveryEurekaApplication.java
@@ -2,7 +2,9 @@ package com.midou.cloud.eureka.discoveryeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
 @EnableEurekaServer
 @SpringBootApplication
