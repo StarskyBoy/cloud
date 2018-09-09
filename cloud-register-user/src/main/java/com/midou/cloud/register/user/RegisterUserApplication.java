b8b24513d3c290f7f26c47e8e03c5f1331c887b2
StarskyBoy <2254493426@qq.com>
2018-09-09 10:58:59 +0800

    eureka-client 服务注册 port=8002

--- cloud-register-user/src/main/java/com/midou/cloud/register/user/RegisterUserApplication.java
+++ cloud-register-user/src/main/java/com/midou/cloud/register/user/RegisterUserApplication.java
@@ -1,8 +1,10 @@
-package com.midou.cloud.register.usermetadata;
+package com.midou.cloud.register.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
 
 @EnableEurekaClient
 @SpringBootApplication
