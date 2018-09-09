09e5f33d43fba191029d717211320cbd454306c4
StarskyBoy <2254493426@qq.com>
2018-09-09 15:30:57 +0800

    开发工具使用IDEA

---
+++ cloud-register-consumer-ribbon-configcustom/src/main/java/com/midou/cloud/register/ribbon/configcustom/Controller/ConsumerController.java
@@ -0,0 +1,34 @@
+package com.midou.cloud.register.ribbon.configcustom.Controller;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author midou
+ * @description
+ * @date 2018/9/3 22:03
+ * @modified by
+ */
+@RestController
+public class ConsumerController {
+
+    private static final org.slf4j.Logger logger = LoggerFactory.getLogger(ConsumerController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/log-user-instance")
+    public void logUserInstance() {
+        logger.info(".......................log-user-instance.....................");
+        ServiceInstance serviceInstance = this.loadBalancerClient.choose("cloud-register-user");
+        logger.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+    }
+}
---
+++ cloud-register-consumer-ribbon/src/main/java/com/midou/cloud/register/consumer/ribbon/RegisterConsumerRibbonApplication.java
@@ -0,0 +1,24 @@
+package com.midou.cloud.register.consumer.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class RegisterConsumerRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterConsumerRibbonApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate()
+    {
+        return  new RestTemplate();
+    }
+}
