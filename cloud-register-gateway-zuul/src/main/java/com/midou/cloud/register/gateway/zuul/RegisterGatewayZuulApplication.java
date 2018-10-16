ef0acccad542f8795087eb847c1d1e05933ef6dd
StarskyBoy <2254493426@qq.com>
2018-10-16 19:38:52 +0800

    编写Zuul微服务网关 port=8023

--- cloud-register-gateway-zuul/src/main/java/com/midou/cloud/register/gateway/zuul/RegisterGatewayZuulApplication.java
+++ cloud-register-gateway-zuul/src/main/java/com/midou/cloud/register/gateway/zuul/RegisterGatewayZuulApplication.java
@@ -2,9 +2,7 @@ package com.midou.cloud.register.gateway.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @EnableZuulProxy
 @SpringBootApplication
