4d7042c2af92405fd6543cf7d7880ee2f100209a
StarskyBoy <2254493426@qq.com>
2018-10-27 16:05:11 +0800

    编写Config Client port=8051

---
+++ cloud-config-client/src/main/java/com/midou/cloud/config/client/CloudConfigClientApplication.java
@@ -0,0 +1,14 @@
+package com.midou.cloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+public class CloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigClientApplication.class, args);
+    }
+}
